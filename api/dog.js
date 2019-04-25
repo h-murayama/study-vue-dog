99b01b825cad8ec49e2dabf4681a56d400137b33
h-murayama <h-murayama@vectorinc.co.jp>
2019-04-25 18:25:20 +0900

    update: api images

--- api/dog.js
+++ api/dog.js
@@ -1,17 +1,31 @@
-import axios from 'axios'
+import axios from "axios";
 
 class DogApi {
-    constructor(){
-        this.apiBase = 'https://dog.ceo/api';
-    }
+  constructor() {
+    this.apiBase = "https://dog.ceo/api";
+  }
 
-    breeds(){
-        return axios.get(`${this.apiBase}/breeds/list/all`)
-            .then(json => {
-                return json.data.message;
-            })
-            .catch(e => ({ error: e }));
-    }
+  breeds() {
+    return axios
+      .get(`${this.apiBase}/breeds/list/all`)
+      .then(json => {
+        return json.data.message;
+      })
+      .catch(e => ({ error: e }));
+  }
+  dogs(breed) {
+    return axios
+      .get(`${this.apiBase}/breed/${breed}/images`)
+      .then(json => {
+        return json.data.message.map(d => {
+          return {
+            url: d, //画像URL
+            like: 0 //良いね数
+          };
+        });
+      })
+      .catch(e => ({ error: e }));
+  }
 }
 
 const dogApi = new DogApi();
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,26 @@
 <template>
-    <section class="container">
-        <div class="columns is-multiline">
-            <div v-for="(item, i) in breed_list" v-bind:key='i' class='column is-2'>
-                <a class="button">{{i}}</a>
-            </div>
-        </div>
-    </section>
+  <section class="container">
+    <div class="columns is-multiline">
+      <div v-for="(item, i) in breed_list" v-bind:key="i" class="column is-2">
+        <!-- <a class="button">{{i}}</a> -->
+        <nuxt-link :to="{path:'dogs/' + i }" class="button">{{i}}</nuxt-link>
+      </div>
+    </div>
+  </section>
 </template>
 
 
 <script>
-    import dogApi from '@/api/dog';
-    import {mapState} from 'vuex';
+import dogApi from "@/api/dog";
+import { mapState } from "vuex";
 
-    export default {
-        async fetch({store}) {
-            let json = await dogApi.breeds();
-            store.commit('breed_list_update', json)
-        },
-        computed: mapState(['breed_list']),
-    }
+export default {
+  async fetch({ store }) {
+    let json = await dogApi.breeds();
+    store.commit("breed_list_update", json);
+  },
+  computed: mapState(["breed_list"])
+};
 </script>
 
 
